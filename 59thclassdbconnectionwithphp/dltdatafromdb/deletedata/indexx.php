bd9dd90500442389095e22fb246f1f990dc01e5c
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-05 12:50:14 +0600

    add age to server

--- 59thclassdbconnectionwithphp/dltdatafromdb/deletedata/indexx.php
+++ 59thclassdbconnectionwithphp/dltdatafromdb/deletedata/indexx.php
@@ -15,16 +15,18 @@
                 <th>ID</th>
                 <th>NAME</th>
                 <th>EMAIL</th>
+                <th>Age</th>
                 <th>ACTION</th>
             </tr>
             <?php
                      
                     $users=$db->query("select * from friend");
-                    while(list($_id,$_name,$_email)=$users->fetch_row()){
+                    while(list($_id,$_name,$_email,$_age)=$users->fetch_row()){
                         echo "<tr>
                             <td>$_id</td>
                             <td>$_name</td>
                             <td>$_email</td>
+                            <td>$_age</td>
                             <td>
                                 <a href='indexx.php?deleteid=$_id'>DELETE</a>
                             </td>
