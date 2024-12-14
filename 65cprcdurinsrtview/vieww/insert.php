17e8c5f863df0e730440b23fa80943c637d9fbc5
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-14 11:43:02 +0600

    push

--- 65cprcdurinsrtview/vieww/insert.php
+++ 65cprcdurinsrtview/vieww/insert.php
@@ -11,12 +11,12 @@
     }//end of inserting formula
 
     //this code will show 
-    // if(isset($_POST['addproduct'])){
-    //     $pname=$_POST['pnam'];
-    //     $pprice=$_POST['pp'];
-    //     $mid=$_POST['product'];
-    //     $dbcon->query("call product_t('$pname','$pprice','$mid')");
-    // }
+    if(isset($_POST['addproduct'])){
+        $pname=$_POST['pnam'];
+        $pprice=$_POST['pp'];
+        $mid=$_POST['product'];
+        $dbcon->query("call pro('$pname','$pprice','$mid')");
+    }
 
 ?>
 <!DOCTYPE html>
