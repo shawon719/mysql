9f1f7a2c553776e4f4f3eb9bc18b2d8e76603508
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-21 23:35:54 +0600

    add product

--- practice/procedureinsert/insert.php
+++ practice/procedureinsert/insert.php
@@ -7,6 +7,13 @@
             $contact=$_POST["co"];
             $db->query("call manu('$manufactureName','$contact')");
         }
+
+        //this code is for binding with the product table properties;
+        if(isset($_POST["addPro"])){
+            $productName=$_POST["pro"];
+            $price=$_POST["pric"];
+            
+        }
 ?>
 
 <!DOCTYPE html>
@@ -17,11 +24,32 @@
     <title>insert,add view,trigger</title>
 </head>
 <body>
-    <form action="" method="post">
-        <h2>this manufacture table</h2>
-        Manufacture Name:<input type="text" name="ma"><br><br>
-        Contact: <input type="text" name="co"><br><br>
-        <button name="addManu">Submit</button>
-    </form>
+    <section>
+        <form action="" method="post">
+            <h2>this manufacture table</h2>
+            Manufacture Name:<input type="text" name="ma"><br><br>
+            Contact: <input type="text" name="co"><br><br>
+            <button name="addManu">Submit</button>
+        </form>
+    </section>
+    <section>
+        <form action="" method="post">
+            <h2>this is add product table</h2>
+            Product Name: <input type="text" name="pro"><br><br>
+            Price:<input type="text" name="pric"><br><br>
+            Manufacture Name: <select name="manufact" id="">
+                <?php
+                        $manu=$db->query("select * from manufacturetabl");
+                        while(list($_mid,$_mname)=$manu->fetch_row()){
+                            echo "<option value='$_mid'>$_mname</option>";
+                        }
+                ?>
+            </select><br><br>
+            <button name="addPro">Add product</button>
+        </form>
+    </section>
+    
+
+
 </body>
 </html>
